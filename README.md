0834664f87dc9968e29c8964dc208d8ffd29fcd6
Huber Dominik <Huberdo@voestalpine.root.local>
2018-07-31 14:14:55 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Logon Extracter
+
+### Wie funktioniert es?
+
+- Kopiere einen Text mit Logons in die obere Textbox
+- Es werden U Logons und V logons automatisch erkannt und das untere Textfeld rein geschrieben, bereit zum kopieren :)
+- Doppel Klicke auf weitere Namen oder Wörter die du ebenfalls in deiner Copy List haben möchtest.
---
+++ index.js
@@ -0,0 +1,51 @@
+const textbox = document.getElementById("textArea");
+
+const output = document.getElementById("output");
+
+// matches just the value + paranthesis
+const regex1 = /[u+v+]\d+/g;
+
+// global array with matches
+let matches = [];
+
+const onTextChange = e => {
+  setTimeout(() => {
+    pastedText = e.target.value;
+
+    // e.target.value.match(regex1)
+    foundLogons = pastedText.match(regex1);
+    console.log("found logons", foundLogons);
+
+    matches = matches.concat(foundLogons);
+
+    renderTextArea();
+  }, 100);
+};
+
+const getSelectionText = () => {
+  var text = "";
+  if (window.getSelection) {
+    text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type != "Control") {
+    text = document.selection.createRange().text.trim();
+  }
+
+  if (!text.replace(/\s/g, "").length) {
+    // string only contained whitespace (ie. spaces, tabs or line breaks)
+    return;
+  }
+
+  // else push the marked text into the array
+  //   debugger;
+  text ? matches.push(text) : null;
+  renderTextArea();
+};
+
+const renderTextArea = () => {
+  console.log(matches);
+  output.value = matches.join("\n");
+};
+
+// Event Listener
+textbox.addEventListener("dblclick", getSelectionText);
+textbox.addEventListener("paste", onTextChange);
